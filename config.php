b6277bd295491caf6ea2f1604ec52b4fab08c51b
Andrew Kramer <andrew@jmpesp.org>
2017-01-31 05:00:51 +0000

    Add snarky message for "hackers" who find the sup3r s3cr3t sql password

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 $mysql_host = "localhost";
 $mysql_username = "trojantransit";
-$mysql_password = "F2MLhfkcnGgb5UZD";
+$mysql_password = "F2MLhfkcnGgb5UZD"; // ya, I know this is public, idgaf
 $mysql_db = "trojantransit";
 
